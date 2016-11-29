c0732185d8dd4ec4a40e6c9add0bc4fb942c88ec
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2016-11-29 16:00:16 -0400

    Modelo article

---
+++ app/modules/models/articlesModel.js
@@ -0,0 +1,25 @@
+(function() {
+	'use strict';
+
+	/**
+	 * @ngdoc function
+	 * @name app.service:articlesService
+	 * @description
+	 * # articlesService
+	 * Service of the app
+	 */
+
+	angular
+		.module('models')
+		.factory('ArticlesModel', Articles);
+
+		Articles.$inject = ['$resource'];
+
+		function Articles ($resource) {
+			var BaseURL = "http://localhost:3000/api";
+			var resourceURL = BaseURL + "/Items/";
+
+			return $resource(resourceURL + '/:id', {id: '@id'});
+		}
+
+	})();
