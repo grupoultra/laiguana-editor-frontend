d1eb8a451db4e94e784cc190bbb72e857f4b1302
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2016-11-29 16:02:11 -0400

    Manejando campos

--- app/modules/articles/articlesCtrl.js
+++ app/modules/articles/articlesCtrl.js
@@ -13,18 +13,65 @@
 		.module('articles')
 		.controller('ArticlesCtrl', Articles);
 
-		Articles.$inject = [];
+		Articles.$inject = ['$scope', 'ArticlesModel', 'CategoriesModel'];
 
-		/*
-		* recommend
-		* Using function declarations
-		* and bindable members up top.
-		*/
-
-		function Articles() {
+		function Articles($scope, ArticlesModel, CategoriesModel) {
 			/*jshint validthis: true */
 			var vm = this;
 
+			vm.tags = [];
+			vm.alt_titles = [];
+			vm.readonly = false;
+			$scope.Categories = [1,2,3,4,5];
+			$scope.Zones = [1,2,3,4,5];
+
+			CategoriesModel.categories(function(data){
+				console.log(data);
+				$scope.Categories = data;
+			});
+
+			CategoriesModel.zones(function(data){
+				$scope.Zones = data;
+			});
+
+
+			$scope.selectedCategories = [];
+			$scope.selectedZones = [];
+			$scope.toggle = function (item, list) {
+				var idx = list.indexOf(item);
+				if (idx > -1) {
+					list.splice(idx, 1);
+				}
+				else {
+					list.push(item);
+				}
+			};
+
+			$scope.exists = function (item, list) {
+				return list.indexOf(item) > -1;
+			};
+
+			$scope.isIndeterminate = function(list) {
+				return ($scope['selected' + list]['length'] !== 0 &&
+				$scope['selected' + list]['length'] !== $scope[list]['length']);
+			};
+
+			$scope.isChecked = function(list) {
+				return $scope['selected' + list]['length'] === $scope[list]['length'];
+			};
+
+			$scope.toggleAll = function(list) {
+				if ($scope['selected' + list]['length'] === $scope[list]['length']) {
+					$scope['selected' + list] = [];
+				} else if ($scope['selected' + list]['length'] === 0 || $scope['selected' + list]['length'] > 0) {
+					$scope['selected' + list] = $scope[list].slice(0);
+				}
+			};
+
+			ArticlesModel.query({filter: {"include":"editorUser"}}).$promise
+				.then(function(data){
+					$scope.articles = data;
+				});
 		}
 
 })();
