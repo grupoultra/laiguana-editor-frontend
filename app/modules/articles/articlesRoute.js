30c37c7cabcd083855a29eaabf69ac857ca5903d
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2016-12-01 15:29:18 -0400

    refactorizacion

--- app/modules/articles/articlesRoute.js
+++ app/modules/articles/articlesRoute.js
@@ -14,19 +14,19 @@ angular.module('articles')
 		$stateProvider
 			.state('home.articles', {
 				url:'/articles',
-				templateUrl: 'app/modules/articles/articles.html',
+				templateUrl: 'app/modules/articles/views/articles.html',
 				controller: 'ArticlesCtrl',
 				controllerAs: 'vm'
 			})
 			.state('home.article', {
 				url:'/article/:id',
-				templateUrl: 'app/modules/articles/article.html',
+				templateUrl: 'app/modules/articles/views/article.html',
 				controller: 'ArticleCtrl',
 				controllerAs: 'vm'
 			})
 			.state('home.newarticle', {
 				url:'/articles/new',
-				templateUrl: 'app/modules/articles/new-article.html',
+				templateUrl: 'app/modules/articles/views/new-article.html',
 				controller: 'ArticlesCtrl',
 				controllerAs: 'vm'
 			});
