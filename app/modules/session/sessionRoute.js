2d331657d7a1b2294e5245421ddedba271c02121
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2016-12-12 16:44:26 -0400

    Modificaciones minimas

--- app/modules/session/sessionRoute.js
+++ app/modules/session/sessionRoute.js
@@ -10,14 +10,14 @@
 
 angular.module('session')
 	.config(['$stateProvider', function ($stateProvider) {
-		
+
 		$stateProvider
-			.state('home.session', {
-				url:'/session',
+			.state('home.login', {
+				url:'/login',
 				templateUrl: 'app/modules/session/session.html',
 				controller: 'SessionCtrl',
 				controllerAs: 'vm'
 			});
 
-		
+
 	}]);
